d6622f5ba9d520c9b2e2d458c19f1df920df9e3d
Franco V <frava46254425mail.com>
2023-10-01 00:14:20 -0300

    Filtrado por Genero

---
+++ src/controllers/getFilterGenre.js
@@ -0,0 +1,26 @@
+const { Genre, Movie } = require("../db");
+
+const getFilterGenre = async(req, res) => {
+    try {
+        const {genre} = req.query;
+        console.log(genre)
+
+        const genreMovies = await Genre.findByPk(genre, {
+            include: [
+                {
+                  model: Movie,
+                  through: { attributes: []},
+                  attributes: ["id", "title", "duration", "description", "image", "year", "language", "torrent" ]
+                }
+            ]
+        })
+
+        res.status(200).json(genreMovies)
+
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
+}
+
+
+module.exports = {getFilterGenre}
--- src/routes/routerMovies.js
+++ src/routes/routerMovies.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { getMovies } = require('../controllers/getMovies');
 const { getByName } = require('../controllers/getByName');
 const { getDetail } = require('../controllers/getDetail');
+const { getFilterGenre } = require('../controllers/getFilterGenre');
 
 /* const { getFilterYear, getFilterLanguage } = require('../controllers/getFilters'); */
 
@@ -9,10 +10,10 @@ const { getDetail } = require('../controllers/getDetail');
 const routerMovies = Router();
 
 
+routerMovies.get("/genre", getFilterGenre)
 routerMovies.get("/", getMovies)
 routerMovies.get("/name", getByName)
 routerMovies.get("/:id", getDetail)
-routerMovies.get("/genders")
 routerMovies.post("/fav",)
 routerMovies.delete("/fav/:id")
 routerMovies.get("/filter/year/:number", /* getFilterYear */)
