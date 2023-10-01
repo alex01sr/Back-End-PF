dbf7e4c6833dc721df1cf4c093a5b254d4d36e95
ThiagoCancellarich <131214817+ThiagoCancellarich@users.noreply.github.com>
2023-10-01 13:35:22 -0300

    Creacion de: getUser , postUser , getGenre

--- src/routes/routerLogin.js
+++ src/routes/routerLogin.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
+const { getUser } = require('../controllers/getUser');
+const { postUser } = require('../controllers/postUser');
 
 
 const routerLogin = Router();
 
-routerLogin.get("/")
-routerLogin.post("/")
+routerLogin.post("/", postUser )
+routerLogin.get("/", getUser )
 
 module.exports = routerLogin
 
