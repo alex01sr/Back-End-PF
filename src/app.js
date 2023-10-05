9eb00def24fbf4325ff4d0cb1115a0f342361ad7
alex01sr <89197642+alex01sr@users.noreply.github.com>
2023-10-05 12:27:33 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const router = require("./Routes/index")
+const router = require("./routes/index")
 
 require('./db.js');
 
